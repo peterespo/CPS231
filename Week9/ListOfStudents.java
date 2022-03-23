8125dca4f23d96be96b8da465c69d02ccd70bdfa
Peter Esposito <peterespo5@yahoo.com>
2022-03-23 17:37:59 -0400

    ListOfStudents

---
+++ Week9/ListOfStudents.java
@@ -0,0 +1,24 @@
+/*
+
+dutchess -> class
+            students
+            faculty
+
+id
+first
+last
+
+Aran Cruz Santiago aran.cruzsantiago@sunydutchess.edu A00000001
+Peter Esposito peter.esposito@sunydutchess.edu A00000002
+Akram Gafur akram.gafur@sunydutchess.edu A00000003
+Caleb Herrera caleb.herrera@sunydutchess.edu A00000004
+John Maksuta john.maksuta@sunydutchess.edu A00000005
+Viridiana Marin Herrera viridiana.marinherre@sunydutchess.edu A00000006
+Brandon Mohammed brandon.mohammed@sunydutchess.edu A00000007
+Christopher Shenton christopher.shenton@sunydutchess.edu A00000008
+Lucas Stoffel lucas.stoffel@sunydutchess.edu A00000009
+*/
+
+public class ListOfStudents {
+    
+}
