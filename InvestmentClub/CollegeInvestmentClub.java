ff1995f535ca639fb4c741a27f6eb6ba9b12cfe6
Peter Esposito <peterespo5@yahoo.com>
2022-04-25 18:38:52 -0400

    getData method added

--- InvestmentClub/CollegeInvestmentClub.java
+++ InvestmentClub/CollegeInvestmentClub.java
@@ -5,14 +5,17 @@
 public class CollegeInvestmentClub {
     public static void main(String[] args) {
         Employee e1 = new Employee("Peter", "Esposito", "10001", "peter.esposito@sunydutchess.edu", 502.81, "Student", "845-391-1293", 1);
-        
-        System.out.println(e1.getFirstName());
-        System.out.println(e1.getLastName());
-        System.out.println(e1.getidNum());
-        System.out.println(e1.getEmail());
-        System.out.println(e1.getBalance());
-        System.out.println(e1.getEmployeeType());
-        System.out.println(e1.getPhoneNumber());
-        
+             
+        getData(e1);
+    }
+    
+    public static void getData(Employee e) {
+        System.out.println(e.getFirstName());
+        System.out.println(e.getLastName());
+        System.out.println(e.getidNum());
+        System.out.println(e.getEmail());
+        System.out.println(e.getBalance());
+        System.out.println(e.getEmployeeType());
+        System.out.println(e.getPhoneNumber());
     }
 }
