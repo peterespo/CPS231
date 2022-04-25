13c5b879b03aa052eb8be004af146267fe4b7f9b
Peter Esposito <peterespo5@yahoo.com>
2022-04-25 18:28:12 -0400

    Printed all things

--- Week10/CollegeInvestmentClub.java
+++ Week10/CollegeInvestmentClub.java
@@ -5,9 +5,14 @@
 public class CollegeInvestmentClub {
     public static void main(String[] args) {
         Employee e1 = new Employee("Peter", "Esposito", "10001", "peter.esposito@sunydutchess.edu", 502.81, "Student", "845-391-1293", 1);
-
-        System.out.println(e1.getBalance());
+        
         System.out.println(e1.getFirstName());
         System.out.println(e1.getLastName());
+        System.out.println(e1.getidNum());
+        System.out.println(e1.getEmail());
+        System.out.println(e1.getBalance());
+        System.out.println(e1.getEmployeeType());
+        System.out.println(e1.getPhoneNumber());
+        
     }
 }
